4548b893ff78b7dfcc1136b584dae3bec5c0876b
haroldalmon <haroldemail@gmail.com>
2015-11-15 11:48:21 -0600

    Start adding the calculations. Works for one target digit.

--- src/P040_ChampernownesConstant.scala
+++ src/P040_ChampernownesConstant.scala
@@ -4,24 +4,39 @@ object P040_ChampernownesConstant {
   }
 
   private def champernownesConstant() {
-    var product = 0
+    var product = 1
     var number = 1
+    var targetDigit = 111
+    println("target characeter = " + targetDigit)
 
     var powers = Array(1, 10, 100, 1000)
+    //println("powers size = " + powers.size)
 
-    for (number <- powers)
-      println(number)
+    /*for (number <- powers)
+      println(number)*/
     var sequence = ""
 
+    //  TODO: Loop through target digits
     number = 1
-    for( i <- 1 to 3) {
+    var seqLength = 0
+    for( noOfSequences <- 1 to 10) {
       sequence = ""
-      for (j <- 1 to 10) {
-    	  sequence = sequence.concat( number.toString() )
+      for (increment <- 1 to 15) {
+    	  sequence = sequence + number.toString
         number += 1
       }
-      println(sequence + ", length = " + sequence.length() )
+      seqLength += sequence.length
+
+      if ( targetDigit <= seqLength && targetDigit > (seqLength - sequence.length) ) {
+        var offset = seqLength - targetDigit
+        println( "offset = " + (seqLength - targetDigit).toString )
+        println( "sequence " + sequence )
+        var digit = sequence.charAt( sequence.length - offset - 1)
+        println( "character = " + digit )
+        product *= digit.toInt - 48
+        // TODO: Get next target digit
+      }
     }
-    
+    println("product = " + product)
   }
 }
