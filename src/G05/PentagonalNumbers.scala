5c704bdea74672fa7c4df5a453752b776478851a
haroldalmon <haroldemail@gmail.com>
2015-11-18 20:20:35 -0600

    Initial version.

---
+++ src/G05/PentagonalNumbers.scala
@@ -0,0 +1,20 @@
+package G05
+
+object PentagonalNumbers {
+  val MAX = 10
+  def main(args: Array[String]) {
+      findNumber();
+  }
+
+  private def findNumber() {
+    var pentagonal = 1 to MAX toArray
+
+    pentagonal = pentagonal.map { x => Pn(x) }
+    pentagonal.foreach { println }
+
+  }
+  
+  private def Pn(n: Int) = {
+    n * ( 3 * n - 1 ) / 2
+  }
+}
