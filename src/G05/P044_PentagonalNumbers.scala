b24d14fc13dcb42ac728a4323948ac8b940b3d84
haroldalmon <haroldemail@gmail.com>
2015-11-20 07:21:54 -0600

    Refactor with functional idioms.

--- src/G05/P044_PentagonalNumbers.scala
+++ src/G05/P044_PentagonalNumbers.scala
@@ -2,6 +2,7 @@ package G05
 
 object PentagonalNumbers {
   val MAX = 3000
+
   def main(args: Array[String]) {
       findNumber();
   }
@@ -10,33 +11,30 @@ object PentagonalNumbers {
     val pentagonal = (1 to MAX).toArray.map { x => Pn(x) }
     val pentaSet = pentagonal.toSet
 
-    var Pj = 0
-    var Pk = 0
-    var diff = 0
-    var sum = 0
-    for (pos1 <- 0 to MAX - 2)
-      for (pos2 <- pos1 + 1 to MAX - 1) {
-        Pj = pentagonal(pos1)
-        Pk = pentagonal(pos2)
-        //Pj.apply { x => printit(x)}
+    (0 to MAX - 2).foreach {
+      pos1 => (pos1 + 1 to MAX - 1).foreach {
+          pos2 => isPentagonal(pos1, pos2)
+        }
+    }
+    
+    def isPentagonal(pos1: Int, pos2: Int) {
+        val Pj = pentagonal(pos1)
+        val Pk = pentagonal(pos2)
         
-        sum = Pj + Pk
+        val sum = Pj + Pk
         if ( pentaSet.contains( sum ) ) {
-          diff = Pk - Pj
-          if ( pentaSet.contains( diff) ) {
-            println("P1 = " + Pj + ", P2 = " + Pk)
-            println ( "The sum " + sum + " is pentagonal" )
+          val diff = Pk - Pj
+          if ( pentaSet.contains( diff ) ) {
             println ( "The diff " + diff + " is pentagonal" )
           }
         }
     }
+
+    /* (0 to 3).foreach {
+       i => println("i = " + i)
+    }*/
   }
-  
-  def printit = {
-    println _
-  }
-  
   private def Pn(n: Int) = {
-    n * ( 3 * n - 1 ) / 2
+    n * (3 * n - 1) / 2
   }
 }
