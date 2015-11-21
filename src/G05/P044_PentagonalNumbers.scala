f4464025337348fe69279e597ccdbd0dbc8836d7
haroldalmon <haroldemail@gmail.com>
2015-11-21 10:12:36 -0600

    Use more of the function idiom and formatting style.

--- src/G05/P044_PentagonalNumbers.scala
+++ src/G05/P044_PentagonalNumbers.scala
@@ -3,34 +3,22 @@ package G05
 object P044_PentagonalNumbers {
   val MAX = 3000
 
-  def main(args: Array[String]) {
-      findNumber();
-  }
+  def main(args: Array[String]) { findNumber(); }
 
   private def findNumber() {
-    val pentagonal = (1 to MAX).toArray.map { x => Pn(x) }
-    val pentagonalSet = pentagonal.toSet
+    val pentagonalNumbers = (1 to MAX).toArray.map { x => Pn(x) }
+    val pentagonalSet = pentagonalNumbers.toSet
 
     (0 to MAX - 2).foreach {
-      pos1 => (pos1 + 1 to MAX - 1).foreach {
-          pos2 => isPentagonal(pos1, pos2)
-        }
+      pos1 => (pos1 + 1 to MAX - 1).foreach { pos2 => isPentagonal(pos1, pos2) }
     }
     
     def isPentagonal(pos1: Int, pos2: Int) {
-        val Pj = pentagonal(pos1)
-        val Pk = pentagonal(pos2)
+        val Pj = pentagonalNumbers(pos1)
+        val Pk = pentagonalNumbers(pos2)
         
-        val sum = Pj + Pk
-        if ( pentagonalSet.contains( sum ) ) {
-          val diff = Pk - Pj
-          if ( pentagonalSet.contains( diff ) ) {
-            println ( "The diff " + diff + " is pentagonal" )
-          }
-        }
+        if ( pentagonalSet.contains( Pj + Pk ) && pentagonalSet.contains( Pk - Pj )) println ( "The diff " + (Pk - Pj) + " is pentagonal" )
     }
   }
-  private def Pn(n: Int) = {
-    n * (3 * n - 1) / 2
-  }
+  private def Pn(n: Int) = { n * (3 * n - 1) / 2 }
 }
