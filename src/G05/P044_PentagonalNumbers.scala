fa9a21110f754e4c6061b5242f825c90cc3060cd
haroldalmon <haroldemail@gmail.com>
2015-11-21 19:32:14 -0600

    Refactor names and formatting style.

--- src/G05/P044_PentagonalNumbers.scala
+++ src/G05/P044_PentagonalNumbers.scala
@@ -1,23 +1,25 @@
 package G05
 
+/** Strategy: Brute Force, Hash Set Lookup */
 object P044_PentagonalNumbers {
-  val MAX = 3000
+  val pentagonalUpperLimit = 3000
 
   def main(args: Array[String]) { findNumber(); }
 
   private def findNumber() {
-    val pentagonalNumbers = (1 to MAX).toArray.map { x => Pn(x) }
+    val pentagonalNumbers = ( 1 to pentagonalUpperLimit).toArray.map { x => Pn( x ) }
     val pentagonalSet = pentagonalNumbers.toSet
 
-    (0 to MAX - 2).foreach {
-      pos1 => (pos1 + 1 to MAX - 1).foreach { pos2 => isPentagonal(pos1, pos2) }
-    }
-    
-    def isPentagonal(pos1: Int, pos2: Int) {
-        val Pj = pentagonalNumbers(pos1)
-        val Pk = pentagonalNumbers(pos2)
-        
-        if ( pentagonalSet.contains( Pj + Pk ) && pentagonalSet.contains( Pk - Pj )) println ( "The diff " + (Pk - Pj) + " is pentagonal" )
+    (0 to pentagonalUpperLimit - 2).
+      foreach { leftIndex => ( leftIndex + 1 to pentagonalUpperLimit - 1).
+      foreach { rightIndex => isPentagonal( leftIndex, rightIndex ) } }
+
+    def isPentagonal( leftIndex: Int, rightIndex: Int ) {
+      val Pj = pentagonalNumbers( leftIndex )
+      val Pk = pentagonalNumbers( rightIndex )
+
+      if (pentagonalSet.contains( Pj + Pk ) && pentagonalSet.contains( Pk - Pj ) ) 
+        println("Minimised difference is " + (Pk - Pj) )
     }
   }
   private def Pn(n: Int) = { n * (3 * n - 1) / 2 }
