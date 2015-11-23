845b7975e568ac5054067cff9c1bbc8be8e21a4f
haroldalmon <haroldemail@gmail.com>
2015-11-22 21:51:16 -0600

    Refactor.

--- src/G05/P045_TriangularPentagonalAndHexagonal.scala
+++ src/G05/P045_TriangularPentagonalAndHexagonal.scala
@@ -8,27 +8,24 @@ object TriangularPentagonalAndHexagonal {
   private def findNumber() {
     for ( i <- 280 to 290) {
       val triangle = Tn( i )
- 		  //println ( i + ", " + triangle )
  		  println( triangle + ", " + isHexagonal( triangle ) )
     }
   }
 
-  private def Tn(n: Int) = {
-    n * (n + 1) / 2
-  }
+  private def Tn(n: Int) = { n * (n + 1) / 2 }
   
-  // TODO return boolean
   private def isHexagonal( Hn: Int ) = {
     val root1 = (1 + Math.sqrt( 1 + 8 * Hn)) / 4
     val root2 = (1 - Math.sqrt( 1 + 8 * Hn)) / 4
-    println ("root1 = " + root1 + ", root2 = " + root2) 
+    //println ("root1 = " + root1 + ", root2 = " + root2) 
     root1 == Math.floor( root1 ) || root2 == Math.floor( root2 ) && root2 > 0
   }
-  
-  private def isPentagonal( Hn: Int ) {
+ 
+  // TODO return boolean
+  private def isPentagonal( Pn: Int ) {
     // TODO: update formula...
-    val root1 = (1 + Math.sqrt( 1 + 8 * Hn)) / 4
-    val root2 = (1 + Math.sqrt( 1 + 8 * Hn)) / 4
+    val root1 = (1 + Math.sqrt( 1 + 8 * Pn)) / 4
+    val root2 = (1 + Math.sqrt( 1 + 8 * Pn)) / 4
     println ("root1 = " + root1 + ", root2 = " + root2) 
   }
   
