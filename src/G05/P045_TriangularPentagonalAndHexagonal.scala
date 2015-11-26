535837e4374be72c71635e5874275151402632ac
haroldalmon <haroldemail@gmail.com>
2015-11-25 20:48:32 -0600

    Fix Int overflow bug.

--- src/G05/P045_TriangularPentagonalAndHexagonal.scala
+++ src/G05/P045_TriangularPentagonalAndHexagonal.scala
@@ -1,43 +1,43 @@
 package G05
-
+/** Strategy: Brute Force, Simple Mathematics */
 object TriangularPentagonalAndHexagonal {
     def main(args: Array[String]) {
     findNumber;
   }
 
   private def findNumber {
-    (3 to 1000).foreach { x =>
+    (286 to 60000).foreach { x =>
       val triangle = Tn( x )
  		  if ( isPentagonal( triangle ) && isHexagonal( triangle ) )  {
- 		    println( "triangle number found = " + triangle )
+ 		    println( f"triangle number found = $triangle for Tn($x), Pn(${PnRoot(triangle)}), Hn(${HnRoot(triangle)})" )
+ 		    return
  		  }
     }
   }
 
-  private def Tn(n: Int):Long = { n * (n + 1) / 2 }
+  private def Tn(n: Long):Long = { n * (n + 1) / 2 }
+  private def PnRoot(Tn: Long):Int = { ((1 + Math.sqrt( 1 + 24 * Tn)) / 6).toInt }
+  private def HnRoot(Tn: Long):Int = { ((1 + Math.sqrt( 1 + 8 * Tn)) / 4).toInt }
   
   private def isHexagonal( Tn: Long ):Boolean = {
-    var root1 = (1 + Math.sqrt( 1 + 8 * Tn)) / 4
-    var root2 = (1 - Math.sqrt( 1 + 8 * Tn)) / 4
-
-    //println ("\nroot1 = " + root1 + ", root2 = " + roroundot2) 
+    val root1 = (1 + Math.sqrt( 1 + 8 * Tn)) / 4
+    val root2 = (1 - Math.sqrt( 1 + 8 * Tn)) / 4
     
-    if ( ((root1 == Math.round( root1 )) ) && root1 > 0  || (root2 == Math.round( root2 )) && (root2 > 0) )
-      return true
-    false
-    
-    //if ( ((root1 == Math.round( root1 )) ) && (root1 > 0) || (root2 == Math.round( root2 )) && (root2 > 0) )
-     // return true
+    if ( ((root1 == Math.round( root1 )) ) && root1 > 0  || 
+        (root2 == Math.round( root2 )) && (root2 > 0) )
+      true
+    else
+      false
   }
  
   private def isPentagonal( Tn: Long ):Boolean = {
-    var root1 = (1 + Math.sqrt( 1 + 24 * Tn)) / 6
-    var root2 = (1 - Math.sqrt( 1 + 24 * Tn)) / 6
+    val root1 = (1 + Math.sqrt( 1 + 24 * Tn)) / 6
+    val root2 = (1 - Math.sqrt( 1 + 24 * Tn)) / 6
     
-    //println ("root1 = " + root1 + ", root2 = " + root2)
-    
-    if ( ((root1 == Math.round( root1 )) ) && root1 > 0  || (root2 == Math.round( root2 )) && (root2 > 0) )
-      return true
-    false
+    if ( ((root1 == Math.round( root1 )) ) && root1 > 0  || 
+        (root2 == Math.round( root2 )) && (root2 > 0) )
+      true
+    else
+      false
   }
 }
