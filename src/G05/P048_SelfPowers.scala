1708d87fd4ebccf03ff0d57ce866710c117120c3
haroldalmon <haroldemail@gmail.com>
2015-11-26 07:19:05 -0600

    Refactor functon name.

--- src/G05/P048_SelfPowers.scala
+++ src/G05/P048_SelfPowers.scala
@@ -3,9 +3,9 @@ package G05
 object P048_SelfPowers {
   val TenDigits = Math.pow(10, 10).toLong
   
-  def main(args: Array[String]) {  println ( "Sum = " + sum ) }
+  def main(args: Array[String]) {  println ( "Sum = " + sumMod10 ) }
 
-  def sum = { (1 to 1000).map { x => powerMod10(x, x) }.sum % TenDigits }
+  def sumMod10 = { (1 to 1000).map { x => powerMod10(x, x) }.sum % TenDigits }
   
   // Only keep track of last 10 digits...
   def powerMod10(base: Int, exponent: Int): Long = {
