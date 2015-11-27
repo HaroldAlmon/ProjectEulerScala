1ffed27428fc12c8058662573259030a4aa67141
haroldalmon <haroldemail@gmail.com>
2015-11-27 09:23:08 -0600

    Refactor spacing.

--- src/G05/P045_TriangularPentagonalAndHexagonal.scala
+++ src/G05/P045_TriangularPentagonalAndHexagonal.scala
@@ -1,28 +1,29 @@
 package G05
 /** Strategy: Brute Force, Simple Mathematics */
 object TriangularPentagonalAndHexagonal {
-    def main(args: Array[String]) {
-    findNumber;
+    def main( args: Array[ String ] ) {
+    println( f"Next triangular number is $findNumber" );
   }
 
-  private def findNumber {
-    (286 to 60000).foreach { x =>
+  private def findNumber:Long = {
+    ( 286 to 60000 ).foreach { x =>
       val triangle = Tn( x )
  		  if ( isPentagonal( triangle ) && isHexagonal( triangle ) )  {
  		    println( f"Tn($x) = Pn(${PnQuadraticRoot(triangle).toInt}) = Hn(${HnQuadraticRoot(triangle).toInt}) = $triangle" )
- 		    return
+ 		    return triangle
  		  }
     }
+    return 0
   }
 
-  private def Tn(n: Long) = n * (n + 1) / 2
-  private def PnQuadraticRoot(Tn: Long):Double = (1 + Math.sqrt( 1 + 24 * Tn)) / 6
-  private def HnQuadraticRoot(Tn: Long):Double = (1 + Math.sqrt( 1 + 8 * Tn)) / 4
+  private def Tn(n: Long) = n * ( n + 1 ) / 2
+  private def PnQuadraticRoot( Tn: Long ):Double = (1 + Math.sqrt( 1 + 24 * Tn ) ) / 6
+  private def HnQuadraticRoot( Tn: Long ):Double = (1 + Math.sqrt( 1 + 8 * Tn  ) ) / 4
 
-  private def isPentagonal(Tn: Long): Boolean = {
+  private def isPentagonal( Tn: Long ): Boolean = {
     val root1 = PnQuadraticRoot( Tn )
 
-    if ( root1 == Math.round(root1) )
+    if ( root1 == Math.round( root1) )
       true
     else
       false
