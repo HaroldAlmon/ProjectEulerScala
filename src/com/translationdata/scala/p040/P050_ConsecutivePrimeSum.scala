12f2540d0832a6cbf2b08be66bea4af8941d5454
haroldalmon <haroldemail@gmail.com>
2015-12-01 17:15:00 -0600

    Add code

--- src/com/translationdata/scala/p040/P050_ConsecutivePrimeSum.scala
+++ src/com/translationdata/scala/p040/P050_ConsecutivePrimeSum.scala
@@ -1,5 +1,35 @@
 package com.translationdata.scala.p040
 
-class ConsecutivePrimeSum {
-  
+import org.junit.Test
+import org.junit.Assert._
+
+object P050_ConsecutivePrimeSum {
+	def getNumber:Int = {
+
+	  return 1
+	}
+
+  def getNumber2:Int = {
+    (134000 to 135000 ).foreach { x =>
+         
+      
+    }
+    return 0
+  }
+
 }
+
+class P050_ConsecutivePrimeSum {
+  @Test def P050_ConsecutivePrimeSum {
+    //val result = P050_ConsecutivePrimeSum
+    P050_ConsecutivePrimeSum
+  }
+}
+  
+class P047_DistinctPrimeFactors2 {
+  @Test def DistinctPrimeFactors {
+    val firstNumber = P047_DistinctPrimeFactors.getNumber
+    println( f"First number is ${firstNumber}" )
+    assertEquals( 134043, firstNumber )
+  }
+}  
