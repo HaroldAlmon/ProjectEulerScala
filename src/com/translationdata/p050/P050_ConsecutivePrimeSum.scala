10ebbcc7311db2856de1ffeea1ae5d3933eb9ce4
haroldalmon <haroldemail@gmail.com>
2015-12-02 14:30:02 -0600

    Refactor change function order. Clean up code.

--- src/com/translationdata/p050/P050_ConsecutivePrimeSum.scala
+++ src/com/translationdata/p050/P050_ConsecutivePrimeSum.scala
@@ -7,35 +7,11 @@ import Math.sqrt
 
 import misc.SieveOfEratosthenes;
 
+/** Strategy: Brute Force, Prime Sieve */
 object P050_ConsecutivePrimeSum {
 	val upperLimit = pow(10, 6).toInt
 	val sieve:SieveOfEratosthenes = new SieveOfEratosthenes( upperLimit )
-  
-def primeSumHelper( primeCandidate:Int, currentPrimeSum:Int, maxPrimeSum:Int, primeCount:Int, maxPrimeCount:Int ):(Int,Int) = {
-	  val primeCandidateSum = primeCandidate + currentPrimeSum
-	  
-    if ( primeCandidateSum >= upperLimit ) {
-    	return (maxPrimeCount, maxPrimeSum)
-    }
-      
-    val (parm1, parm2, parm3, parm4) = if ( sieve.isPrime(primeCandidate) == false )
-      ( currentPrimeSum, maxPrimeSum, primeCount, maxPrimeCount )
-    else {
-      if( sieve.isPrime( primeCandidateSum ) ) {
-        ( primeCandidateSum, primeCandidateSum, primeCount + 1, primeCount + 1)
-      }
-      else
-        ( primeCandidateSum, maxPrimeSum, primeCount + 1, maxPrimeCount)
-    }
-	  primeSumHelper( primeCandidate + 1, parm1, parm2, parm3, parm4 )
-  }
-  
-  def primeSum(firstPrime:Int) = {
-    val maxSum = 0 
-    val maxPrimeSum = 0
-    primeSumHelper(firstPrime, maxSum, maxPrimeSum, 0, 0 )
-  }
-
+	
   def getMaxSum:Int = {
     val primeList = extractPrimes( (2 to 10) )
     val primeNumberSums = primeList.toArray.map { firstPrime => primeSum (firstPrime) }
@@ -44,13 +20,37 @@ def primeSumHelper( primeCandidate:Int, currentPrimeSum:Int, maxPrimeSum:Int, pr
       val tup = primeNumberSums(primePos)
       printf ("Starting Prime = %d, Sum = %d, number of terms = %d\n", primeList(primePos), tup._2, tup._1 )
     }
+
     val counts = primeNumberSums.map( tup => tup._1)
     val maxCount = counts.max
     val maxPos = counts.indexWhere(_ == maxCount)
-    
     val maxSum = primeNumberSums(maxPos)._2
-    
     maxSum
+  }
+	
+	def primeSum(firstPrime:Int) = {
+    val maxSum = 0 
+    val maxPrimeSum = 0
+    primeSumTailRecursion(firstPrime, maxSum, maxPrimeSum, 0, 0 )
+  }
+  
+  def primeSumTailRecursion( primeCandidate:Int, currentPrimeSum:Int, maxPrimeSum:Int, primeCount:Int, maxPrimeCount:Int ): (Int,Int) = {
+	  val primeCandidateSum = primeCandidate + currentPrimeSum
+	  
+    if ( primeCandidateSum >= upperLimit ) {
+    	return (maxPrimeCount, maxPrimeSum)
+    }
+      
+    val (parm1Sum, parm2MaxSum, parm3Count, parm4MaxCount) = if ( sieve.isPrime(primeCandidate) == false )
+      ( currentPrimeSum, maxPrimeSum, primeCount, maxPrimeCount )
+    else {
+      if( sieve.isPrime( primeCandidateSum ) ) {
+        ( primeCandidateSum, primeCandidateSum, primeCount + 1, primeCount + 1)
+      }
+      else
+        ( primeCandidateSum, maxPrimeSum, primeCount + 1, maxPrimeCount)
+    }
+	  primeSumTailRecursion( primeCandidate + 1, parm1Sum, parm2MaxSum, parm3Count, parm4MaxCount )
   }
   
   def extractPrimes(candiates:Range.Inclusive) = {
