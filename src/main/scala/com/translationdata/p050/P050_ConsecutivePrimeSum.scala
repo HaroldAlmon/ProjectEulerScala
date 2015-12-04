5d4621622b856c3ff135a8a57ea5c33f394736bb
haroldalmon <haroldemail@gmail.com>
2015-12-04 07:46:20 -0600

    Move lambda expressions into functions

--- src/main/scala/com/translationdata/p050/P050_ConsecutivePrimeSum.scala
+++ src/main/scala/com/translationdata/p050/P050_ConsecutivePrimeSum.scala
@@ -12,18 +12,25 @@ object P050_ConsecutivePrimeSum {
 	val upperLimit = pow(10, 6).toInt
 	val sieve:SieveOfEratosthenes = new SieveOfEratosthenes( upperLimit )
 
+  def mapTermCount( primeTuple: ( Int, Int ) ) =
+    primeTuple._1
+
+  def mapPrimeNumberSum( firstPrime:Int ) =
+    primeSum(firstPrime)
+
   def getMaxSum:Int = {
     val primeList = extractPrimes( ( 2 to 7 ) )
-    val primeNumberSums = primeList.toArray.map { firstPrime => primeSum ( firstPrime ) }
-    
+    val primeNumberSums = primeList.toArray.map { mapPrimeNumberSum }
+
+
     for ( primePos <- 0 to primeNumberSums.size - 1) {
       val primeTuple = primeNumberSums(primePos)
       printf ( "Starting Prime = %d, Sum = %d, number of terms = %d\n", primeList(primePos), primeTuple._2, primeTuple._1 )
     }
 
-    val counts = primeNumberSums.map( primeTuple => primeTuple._1)
-    val maxCount = counts.max
-    val maxPos = counts.indexWhere( _ == maxCount)
+    val termCount = primeNumberSums.map( mapTermCount )
+    val maxCount = termCount.max
+    val maxPos = termCount.indexWhere( _ == maxCount)
     val maxSum = primeNumberSums( maxPos )._2
     maxSum
   }
