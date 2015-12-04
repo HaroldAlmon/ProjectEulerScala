82370ae9d98e17d6219e989963456dfee2bedec7
haroldalmon <haroldemail@gmail.com>
2015-12-04 07:49:26 -0600

    Formmat code

--- src/main/scala/com/translationdata/p050/P050_ConsecutivePrimeSum.scala
+++ src/main/scala/com/translationdata/p050/P050_ConsecutivePrimeSum.scala
@@ -9,14 +9,14 @@ import misc.SieveOfEratosthenes;
 
 /** Strategy: Brute Force, Prime Sieve */
 object P050_ConsecutivePrimeSum {
-	val upperLimit = pow(10, 6).toInt
+	val upperLimit = pow( 10, 6 ).toInt
 	val sieve:SieveOfEratosthenes = new SieveOfEratosthenes( upperLimit )
 
   def mapTermCount( primeTuple: ( Int, Int ) ) =
     primeTuple._1
 
   def mapPrimeNumberSum( firstPrime:Int ) =
-    primeSum(firstPrime)
+    primeSum( firstPrime )
 
   def getMaxSum:Int = {
     val primeList = extractPrimes( ( 2 to 7 ) )
@@ -24,7 +24,7 @@ object P050_ConsecutivePrimeSum {
 
 
     for ( primePos <- 0 to primeNumberSums.size - 1) {
-      val primeTuple = primeNumberSums(primePos)
+      val primeTuple = primeNumberSums( primePos )
       printf ( "Starting Prime = %d, Sum = %d, number of terms = %d\n", primeList(primePos), primeTuple._2, primeTuple._1 )
     }
 
@@ -43,26 +43,26 @@ object P050_ConsecutivePrimeSum {
     primeSumTailRecursion( firstPrime, maxSum, maxPrimeSum, primeCount, maxPrimeCount )
   }
   
-  def primeSumTailRecursion( primeCandidate:Int, currentPrimeSum:Int, maxPrimeSum:Int, primeCount:Int, maxPrimeCount:Int ): (Int,Int) = {
+  def primeSumTailRecursion( primeCandidate:Int, currentPrimeSum:Int, maxPrimeSum:Int, primeCount:Int, maxPrimeCount:Int ): ( Int,Int ) = {
 	  val primeCandidateSum = primeCandidate + currentPrimeSum
 	  
     if ( primeCandidateSum >= upperLimit )
-    	return (maxPrimeCount, maxPrimeSum)
+    	return ( maxPrimeCount, maxPrimeSum )
       
-    val (parm1Sum, parm2MaxSum, parm3Count, parm4MaxCount) = if ( sieve.isPrime(primeCandidate) == false )
+    val ( parm1Sum, parm2MaxSum, parm3Count, parm4MaxCount ) = if ( sieve.isPrime(primeCandidate) == false )
       ( currentPrimeSum, maxPrimeSum, primeCount, maxPrimeCount )
     else {
       if( sieve.isPrime( primeCandidateSum ) ) {
-        ( primeCandidateSum, primeCandidateSum, primeCount + 1, primeCount + 1)
+        ( primeCandidateSum, primeCandidateSum, primeCount + 1, primeCount + 1 )
       }
       else
-        ( primeCandidateSum, maxPrimeSum, primeCount + 1, maxPrimeCount)
+        ( primeCandidateSum, maxPrimeSum, primeCount + 1, maxPrimeCount )
     }
 	  primeSumTailRecursion( primeCandidate + 1, parm1Sum, parm2MaxSum, parm3Count, parm4MaxCount )
   }
   
-  def extractPrimes(candiates:Range.Inclusive) = {
-    for ( num <- candiates if sieve.isPrime(num) )
+  def extractPrimes(candidates:Range.Inclusive) = {
+    for ( num <- candidates if sieve.isPrime(num) )
         yield num
   }
 }
