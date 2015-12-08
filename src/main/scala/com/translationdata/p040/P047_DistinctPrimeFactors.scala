f1af79b8642053f32f33559bfe04b6965ae65274
haroldalmon <haroldemail@gmail.com>
2015-12-07 19:26:15 -0600

    Clean up code.

--- src/main/scala/com/translationdata/p040/P047_DistinctPrimeFactors.scala
+++ src/main/scala/com/translationdata/p040/P047_DistinctPrimeFactors.scala
@@ -4,8 +4,7 @@ import org.junit.Test
 import org.junit.Assert._
 
 /** Strategy: Brute Force, Prime Sieve */
-/* TODO: Make this run faster. This takes over 3 minutes to run. */
-  
+
 object P047_DistinctPrimeFactors {
 	val sieve:SieveOfEratosthenes = new SieveOfEratosthenes( 135000 )
   def main(args: Array[String]) { 
@@ -15,17 +14,17 @@ object P047_DistinctPrimeFactors {
   def getNumber:Int = {
     (134000 to 135000 ).foreach { x =>
       val factors1 = findFactors(x)
-      val factors2 = if(factors1.size == 4) findFactors(x + 1) else (0 to 0)
-      val factors3 = if(factors2.size == 4) findFactors(x + 2) else (0 to 0)
-      val factors4 = if(factors3.size == 4) findFactors(x + 3) else (0 to 0)
+      val factors2 = if(factors1.size == 4) findFactors(x + 1) else 0 to 0
+      val factors3 = if(factors2.size == 4) findFactors(x + 2) else 0 to 0
+      val factors4 = if(factors3.size == 4) findFactors(x + 3) else 0 to 0
       
       
       if ( factors1.size + factors2.size + factors3.size + factors4.size == 16) {
         println (f"Four consequetive nums: ${x}, ${x+1}, ${x+2}, ${x+3}\n")
       
-        printf( "%d factors are %s\n", x,     factors1.mkString(" ") ) 
-        printf( "%d factors are %s\n", x + 1, factors2.mkString(" ") ) 
-        printf( "%d factors are %s\n", x + 2, factors3.mkString(" ") )
+        printf( "%d factors are %s\n", x,       factors1.mkString(" ") )
+        printf( "%d factors are %s\n", x + 1,   factors2.mkString(" ") )
+        printf( "%d factors are %s\n", x + 2,   factors3.mkString(" ") )
         printf( "%d factors are %s\n\n", x + 3, factors4.mkString(" ") )
         return x
       }
