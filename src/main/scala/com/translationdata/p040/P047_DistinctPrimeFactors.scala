bf056d04c169a879f490afbdd459a5c766697776
haroldalmon <haroldemail@gmail.com>
2015-12-11 10:14:00 -0600

    Refactor into functions

--- src/main/scala/com/translationdata/p040/P047_DistinctPrimeFactors.scala
+++ src/main/scala/com/translationdata/p040/P047_DistinctPrimeFactors.scala
@@ -12,16 +12,27 @@ object P047_DistinctPrimeFactors {
   }
 
   def getNumber:Int = {
+    def distinctFactorCount( x:Int ) = {
+      findFactors( x ).size
+    }
+
+    def findFactors( x: Int ):IndexedSeq[Int] = {
+      val factors:IndexedSeq[Int] = for ( factor <- ( 1 to x/2 ) if sieve.isPrime(factor) && ( x % factor == 0 ) )
+        yield factor
+
+      factors
+    }
+
     (134000 to 135000 ).foreach { x =>
       val factors1 = findFactors(x)
       val factors2 = if(factors1.size == 4) findFactors(x + 1) else 0 to 0
       val factors3 = if(factors2.size == 4) findFactors(x + 2) else 0 to 0
       val factors4 = if(factors3.size == 4) findFactors(x + 3) else 0 to 0
-      
-      
+
+
       if ( factors1.size + factors2.size + factors3.size + factors4.size == 16) {
         println (f"Four consequetive nums: ${x}, ${x+1}, ${x+2}, ${x+3}\n")
-      
+
         printf( "%d factors are %s\n", x,       factors1.mkString(" ") )
         printf( "%d factors are %s\n", x + 1,   factors2.mkString(" ") )
         printf( "%d factors are %s\n", x + 2,   factors3.mkString(" ") )
@@ -30,17 +41,6 @@ object P047_DistinctPrimeFactors {
       }
     }
     return 0
-  }
-  
-  def distinctFactorCount( x:Int ) = {
-    findFactors( x ).size
-  }
-
-  def findFactors( x: Int ):IndexedSeq[Int] = {
-    val factors:IndexedSeq[Int] = for ( factor <- ( 1 to x/2 ) if sieve.isPrime(factor) && ( x % factor == 0 ) )
-       yield factor
-      
-    factors
   }
 }
 
