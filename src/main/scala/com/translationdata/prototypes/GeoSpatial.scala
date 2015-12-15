9ae7a36c8577d1211c0eb8a617292c91abaf485a
haroldalmon <haroldemail@gmail.com>
2015-12-15 12:38:59 -0600

    New prototype files

---
+++ src/main/scala/com/translationdata/prototypes/GeoSpatial.scala
@@ -0,0 +1,23 @@
+package com.translationdata.prototypes
+
+object Geospatial {
+  // Latitude = -90 to +90
+  // Longitude = -180 to +180
+def calculateDistance(
+   coordinate1Longitude: Double,
+   coordinate1Latitude: Double,
+   coordinate2Longitude: Double,
+   coordinate2Latitude: Double
+ ): Double = {
+    //implementation will be provided later
+  0.00
+  }
+
+  def calculateCoordinate(
+   coordinate1Longitude: Double,
+   coordinate1Latitude: Double,
+   angle: Double,
+   distance: Double
+ ): (Double, Double) = (0.00, 0.00)
+}
+
---
+++ src/main/scala/com/translationdata/prototypes/factorial.scala
@@ -0,0 +1,8 @@
+package com.translationdata.prototypes
+
+/**
+ * Created by Harold on 12/4/2015.
+ */
+object factorial {
+  
+}
