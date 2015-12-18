3aaf085f25b7bced5466f4a8c4976e07f64ce91f
haroldalmon <haroldemail@gmail.com>
2015-12-18 12:23:07 -0600

    Refactor to nest functions

--- src/main/scala/com/translationdata/p050/P053_CombinatoricSelections.scala
+++ src/main/scala/com/translationdata/p050/P053_CombinatoricSelections.scala
@@ -15,28 +15,30 @@ object P053_CombinatoricSelections {
   def combination( n:Int, r:Int ) = {
     factorial( n, n - r ) / factorial( r )
   }
-  //================================
+
   def factorial(n:Int, r:Int): Long = {
+    def factorialImpl(n:Int, r: Int, product: Long): Long = {
+      if (n <= r) {
+        product
+      } else
+      factorialImpl(n - 1, r, n * product)
+    }
+
     factorialImpl(n, r, 1)
   }
 
-  def factorialImpl(n:Int, r: Int, product: Long): Long = {
-    if (n <= r) {
-      product
-    } else
-      factorialImpl(n - 1, r, n * product)
-  }
-  //================================
   def factorial(n: Int): Long = {
+
+    def factorialImpl(n:Int, product:Long): Long = {
+      if (n <= 1) {
+        product
+      } else
+      factorialImpl(n - 1, n * product)
+    }
+
     factorialImpl(n, 1)
   }
 
-  def factorialImpl(n:Int, product:Long): Long = {
-    if (n <= 1) {
-      product
-    } else
-      factorialImpl(n - 1, n * product)
-  }
 }
 
 class P053_CombinatoricSelections {
