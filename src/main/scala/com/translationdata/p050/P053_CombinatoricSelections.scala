215e2fa07e2acfdb18c7a3017e8ab7fbfd2e4be8
haroldalmon <haroldemail@gmail.com>
2015-12-18 12:43:40 -0600

    Change result type to BigInt to handle large factorial products

--- src/main/scala/com/translationdata/p050/P053_CombinatoricSelections.scala
+++ src/main/scala/com/translationdata/p050/P053_CombinatoricSelections.scala
@@ -4,45 +4,43 @@ import org.junit.Test
 import org.junit.Assert._
 
 object P053_CombinatoricSelections {
-  def getCount(n: Int, upperLimit: Int) = {
-    val result = (1 to n).
-      map { n => for (r <- 1 to n if combination(n, r) > upperLimit)
+  def getCount( n: Int, upperLimit: Int ) = {
+    val result = ( 1 to n ).
+      map { n => for ( r <- 1 to n if combination( n, r ) > upperLimit )
                     yield 1  }.
       map( x => x.size ).sum
     result
   }
 
   def combination( n:Int, r:Int ) = {
-    factorial( n, n - r ) / factorial( r )
+    binomialCoefficient( n, n - r ) / factorial( r )
   }
 
-  def factorial(n:Int, r:Int): Long = {
-    def factorialImpl(n:Int, r: Int, product: Long): Long = {
-      if (n <= r) {
+  def binomialCoefficient(n:Int, r:Int ) = {
+    def factorialImpl( n:Int, r: Int, product: BigInt ): BigInt =
+      if ( n <= r )
         product
-      } else
-      factorialImpl(n - 1, r, n * product)
-    }
+      else
+        factorialImpl( n - 1, r, BigInt( n ) * product )
 
     factorialImpl(n, r, 1)
   }
 
-  def factorial(n: Int): Long = {
-
-    def factorialImpl(n:Int, product:Long): Long = {
-      if (n <= 1) {
+  def factorial( n: Int ) = {
+    def factorialImpl( n:Int, product:BigInt ): BigInt =
+      if ( n <= 1 )
         product
-      } else
-      factorialImpl(n - 1, n * product)
-    }
+      else
+        factorialImpl ( n - 1, BigInt( n ) * product )
 
-    factorialImpl(n, 1)
+    factorialImpl( n, 1 )
   }
-
 }
 
 class P053_CombinatoricSelections {
   @Test def CombinatoricSelections {
-    println( P053_CombinatoricSelections.getCount( 50, 1000000 ) )
+    val count = P053_CombinatoricSelections.getCount( n = 100, upperLimit = 1000000 )
+    println( count )
+    assertEquals( 4075, count )
   }
 }
