a972e9304f51b0d2ab95a7ae9f21c3fbd4b70289
haroldalmon <haroldemail@gmail.com>
2015-12-18 13:04:37 -0600

    Refactor function name.

--- src/main/scala/com/translationdata/p050/P053_CombinatoricSelections.scala
+++ src/main/scala/com/translationdata/p050/P053_CombinatoricSelections.scala
@@ -2,28 +2,24 @@ package com.translationdata.p050
 
 import org.junit.Test
 import org.junit.Assert._
-
+/** Strategy: Brute Force, Simple Math */
 object P053_CombinatoricSelections {
   def getCount( n: Int, upperLimit: Int ) = {
     val result = ( 1 to n ).
-      map { n => for ( r <- 1 to n if combination( n, r ) > upperLimit )
-                    yield 1  }.
-      map( x => x.size ).sum
+      map( n => for ( r <- 1 to n if combination( n, r ) > upperLimit ) yield 1 ).
+      map( vector => vector.size ).sum
     result
   }
 
-  def combination( n:Int, r:Int ) = {
-    binomialCoefficient( n, n - r ) / factorial( r )
-  }
+  def combination( n:Int, r:Int ) = binomialCoefficient( n, n - r ) / factorial( r )
 
-  def binomialCoefficient(n:Int, r:Int ) = {
-    def factorialImpl( n:Int, r: Int, product: BigInt ): BigInt =
-      if ( n <= r )
-        product
+  def binomialCoefficient( n:Int, nMinusR:Int ) = {
+    def binomialCoefficientImpl( n:Int, r: Int, product: BigInt ): BigInt =
+      if ( n <= r )product
       else
-        factorialImpl( n - 1, r, BigInt( n ) * product )
+        binomialCoefficientImpl( n - 1, r, BigInt( n ) * product )
 
-    factorialImpl(n, r, 1)
+    binomialCoefficientImpl(n, nMinusR, 1)
   }
 
   def factorial( n: Int ) = {
@@ -40,7 +36,7 @@ object P053_CombinatoricSelections {
 class P053_CombinatoricSelections {
   @Test def CombinatoricSelections {
     val count = P053_CombinatoricSelections.getCount( n = 100, upperLimit = 1000000 )
-    println( count )
+    printf( "P053: Count = %d\n", count )
     assertEquals( 4075, count )
   }
 }
