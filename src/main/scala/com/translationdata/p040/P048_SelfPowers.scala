be16fc3a4bc23edd5fd79c7b8c6d03eadf2cb495
haroldalmon <HaroldAlmon@users.noreply.github.com>
2015-12-21 17:35:31 -0600

    Refactor to use tail recursion

--- src/main/scala/com/translationdata/p040/P048_SelfPowers.scala
+++ src/main/scala/com/translationdata/p040/P048_SelfPowers.scala
@@ -1,22 +1,30 @@
 package com.translationdata.p040
 import org.junit.Test
 import org.junit.Assert._
+import scala.annotation.tailrec
+
+/** Strategy : Brute Force, Tail Recursion */
 object P048_SelfPowers {
   val tenDigits = Math.pow( 10, 10 ).toLong
   
   def main( args: Array[ String ] ) {  println ( "Last ten digits = " + sumMod10 ) }
 
-  def sumMod10 = { (1 to 1000).map {
+  def sumMod10 = (1 to 1000).map {
     def powerMod10( base: Int, exponent: Int ): Long = {
-      if ( exponent == 0 )
-        1
-      else
-        ( base * powerMod10( base, exponent - 1) ) % tenDigits
+
+      @tailrec
+      def powerMod10Impl( base: Int, exponent: Int, product: Long ): Long = {
+        if ( exponent == 0 )
+          product
+        else
+          powerMod10Impl(base, exponent - 1, base * product % tenDigits)
+      }
+
+      powerMod10Impl(base, exponent, 1)
     }
 
-    x => powerMod10( x, x ) }.sum % tenDigits
     // Only keep track of last 10 digits...
-  }
+    x => powerMod10( x, x ) }.sum % tenDigits
 }
 
 class P048_SelfPowers {
