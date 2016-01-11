3786f20301681b665fba534cbdf7bb6bddbee54e
haroldalmon <HaroldAlmon@users.noreply.github.com>
2016-01-11 10:03:30 -0600

    Add JUnit test. USe Option to handle no value

--- src/main/scala/com/translationdata/p040/P044_PentagonalNumbers.scala
+++ src/main/scala/com/translationdata/p040/P044_PentagonalNumbers.scala
@@ -1,23 +1,26 @@
 package com.translationdata.p040
 
+import org.junit.Test
+import org.junit.Assert._
+
 /** Strategy: Brute Force, Hash Set Lookup */
 object P044_PentagonalNumbers {
   val pentagonalUpperLimit = 3000
 
   def main(args: Array[String]) { findNumber() }
 
-  private def findNumber() {
+  private def findNumber() = {
     val Pn  = (n: Int) => { n * (3 * n - 1) / 2 }
     val pentagonalNumbers = ( 1 to pentagonalUpperLimit ).toArray.map { Pn }
     val pentagonalSet = pentagonalNumbers.toSet
 
-    def findPair:(Long, Int, Int, Int, Int) = {
+    def findPair:Option[(Int, Int, Int, Int, Int)] = {
       (0 to pentagonalUpperLimit - 2).
         foreach { leftIndex => for (rightIndex <- leftIndex + 1 to pentagonalUpperLimit - 1
           if isPentagonal(pentagonalNumbers(leftIndex), pentagonalNumbers(rightIndex)))
-            return ( pentagonalNumbers( rightIndex ) - pentagonalNumbers( leftIndex ), leftIndex, rightIndex, pentagonalNumbers( leftIndex ), pentagonalNumbers( rightIndex ))
+            return Some(( pentagonalNumbers( rightIndex ) - pentagonalNumbers( leftIndex ), leftIndex, rightIndex, pentagonalNumbers( leftIndex ), pentagonalNumbers( rightIndex )))
         }
-      (0L, 0, 0, 0, 0)
+      None
     }
 
     def isPentagonal( Pj: Int, Pk: Int ) =
@@ -26,8 +29,18 @@ object P044_PentagonalNumbers {
       else
         false
 
-    val result = findPair
-    printf("P044:\n")
-    printf("|Pk - Pj| = %d, j = %d, k = %d, Pj = %d, Pk = %d\n", result._1, result._2, result._3, result._4, result._5)
+    val result = findPair.get
+    printf("|Pk - Pj| = %d, j = %d, k = %d, Pj = %d, Pk = %d\n\n", result._1, result._2, result._3, result._4, result._5)
+    result._1
+  }
+
+}
+
+class P044_PentagonalNumbers {
+  @Test def PentagonalNumbers() = {
+    val difference = P044_PentagonalNumbers.findNumber()
+
+    println( "P044: Difference is " + difference )
+    assertEquals(5482660, difference)
   }
 }
