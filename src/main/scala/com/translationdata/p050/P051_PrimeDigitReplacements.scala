5d143a6202f195573aa801b3b56c5a0595414298
haroldalmon <HaroldAlmon@users.noreply.github.com>
2016-01-13 09:13:22 -0600

    Rewite code from scratch

--- src/main/scala/com/translationdata/p050/P051_PrimeDigitReplacements.scala
+++ src/main/scala/com/translationdata/p050/P051_PrimeDigitReplacements.scala
@@ -1,6 +1,29 @@
 package com.translationdata.p050
 
 object P051_PrimeDigitReplacements {
-  def main(args: Array[String]): Unit = {
+
+ val positions = Array(1,2,3,4)
+
+ def main(args: Array[String]): Unit = {
+   positions.foreach(println)
+   printCombinations(positions, 4, 2)
  }
+
+  def printCombinations (positions: Array[Int], n: Int, r:Int): Unit = {
+    printf("n = %d, r = %d\n", n, r)
+
+    printCombinationsImpl(positions, n, r, 0, "")
+  }
+
+  def printCombinationsImpl (positions: Array[Int], n: Int, r:Int, startPos:Int, str: String): Unit = {
+    for (i <- startPos to n - r + 1) {
+      if (r - startPos <= 1) {
+        for(j <- startPos to n - 1)
+          printf("%s\n", str + positions(j))
+        return
+      }
+      else
+        printCombinationsImpl(positions, n, r, i + 1,  str + positions(i))
+    }
+  }
 }
