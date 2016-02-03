0e9a60fee684741b6959d8a912361f7480f994e9
haroldalmon <HaroldAlmon@users.noreply.github.com>
2016-02-02 22:23:03 -0600

    Refactor names

--- src/main/scala/com/translationdata/prototypes/SingleArgumentMonad.scala
+++ src/main/scala/com/translationdata/prototypes/SingleArgumentMonad.scala
@@ -17,12 +17,12 @@ class SingleArgumentMonad[A](val value: A) {
 
 object SingleArgumentMonad {
   def main(args: Array[String]) {
-    val monadWith2 = new SingleArgumentMonad(2)
-    val monadWith3 = new SingleArgumentMonad(3)
+    val monadWith4 = new SingleArgumentMonad(4)
+    val monadWith5 = new SingleArgumentMonad(5)
 
     val forResult = for {
-      x <- monadWith2 // The flatMap function reads the value instance variable fom the monad
-      y <- monadWith3 // The map function reads the value instance variable fom the monad
+      x <- monadWith4 // The flatMap function reads the value instance variable fom the monad
+      y <- monadWith5 // The map function reads the value instance variable fom the monad
     } yield x + y
 
     println("for() result = " + forResult)
@@ -33,48 +33,61 @@ object SingleArgumentMonad {
 
     // Step 1. Convert for to flatMap...
     val flatMapAndForResult =
-      monadWith2.flatMap { x =>
+      monadWith4.flatMap { x =>
         for {
-          y <- monadWith3
+          y <- monadWith5
         } yield x + y
       }
     println("flatMap() + for() result = " + flatMapAndForResult)
 
     // Step 2. Convert for to map...
     val flatMapAndMapResult =
-      monadWith2.flatMap { x =>
-        monadWith3.map { y => x + y }
+      monadWith4.flatMap { x =>
+        monadWith5.map { y => x + y }
       }
 
     val flatMapAndMapresultNoReturnMonad =
-      monadWith2.flatMapNoReturnMonad { x =>
-        monadWith3.flatMapNoReturnMonad { y => x + y }
+      monadWith4.flatMapNoReturnMonad { x =>
+        monadWith5.flatMapNoReturnMonad { y => x + y }
       }
 
-    var cc = monadWith2.map(x => 2 * x)
-    cc = cc.map(x => x * x)
+    var monadValue = monadWith4.map(x => 2 * x)
+    monadValue = monadValue.map(x => x * x)
 
-    var cc2 = monadWith2.flatMapNoReturnMonad(x => 2 * x)
-    cc2 = cc2.flatMapNoReturnMonad(x => x * x)
+    var intValue = monadWith4.flatMapNoReturnMonad(x => 2 * x)
+    //  intValue = intValue.flatMapNoReturnMonad(x => x * x)  // COMPILER ERROR, Int() does not have this function
 
-    flatMapAndMapResult.map(x => x*x)
-    flatMapAndMapResult.flatMap(x => new SingleArgumentMonad(x*x*2) )
+    flatMapAndForResult.map(x => x*x)
+    flatMapAndForResult.flatMap(x => new SingleArgumentMonad(x*x*2) )
 
     // Based on what I see in the debugger this is how the code works.
-    // 1. Execute the monadWith2.flatMap() function. This steps takes the 2
-    // in the monad and substitutes the 2 into monadWith3.map { y => x + y }
-    // which yields monadWith3.map { y => 2 + y }.
+    // 1. Execute the monadWith4.flatMap() function. This steps takes the 4
+    // in the monad and substitutes the 4 into monadWith4.map { y => x + y }
+    // which yields monadWith4.map { y => 4 + y }.
     //
-    // 2. Execute the monadWith3.map() function.  This step takes the 3
-    // in the monad and substitutes the 3 into y => 2 + y which yields 3 => 2 + 3
+    // 2. Execute the monadWith4.map() function.  This step takes the 5
+    // in the monad and substitutes the 5 into y => 2 + y which yields 5 => 2 + 5
     //
-    // 3. Evaluate 2 + 3 which yields 5. So 5 is the result BUT the monad member function in the class
-    // wraps the 5 in a SingleArgumentMonad. So the flatMapAndMapResult variable ends up containing a monad
-    // that contains a 5.
+    // 3. Evaluate 5 + 5 which yields 9. So 9 is the result BUT the monad member function in the class
+    // wraps the 9 in a SingleArgumentMonad. So the flatMapAndMapResult variable ends up containing a monad
+    // that contains a 9.
 
     // The constructor defines value as a val so we can read the data in the monad...
-    val result = flatMapAndMapResult.value
+    val result = flatMapAndForResult.value
     println("flatMap() + map() result = " + result)
+
+    // Two map() calls...
+    val TwoMapsResult =
+      monadWith4.map { x =>
+        monadWith5.map { y => x + y }
+      }
+
+    // Two flatMap() calls...
+    var twoFlatMapsResult =
+      monadWith4.flatMapNoReturnMonad { x =>
+        monadWith5.flatMapNoReturnMonad { y => x + y }
+      }
+    twoFlatMapsResult = null
   }
 }
 
