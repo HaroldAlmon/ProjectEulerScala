1c19ca27fe16fe293197a4934cdaa509aa97673c
Harold Almon <haroldemail@gmail.com>
2016-05-01 11:40:48 -0500

    Update prototype

--- src/main/scala/com/translationdata/p050/P051_Prototype.scala
+++ src/main/scala/com/translationdata/p050/P051_Prototype.scala
@@ -2,26 +2,32 @@ package com.translationdata.p050
 import org.junit.Test
 import org.junit.Assert._
 
-object P051_Prototype {
-  def combinations(n:Int, r:Int):Unit = {
-    /*    val elements = Array('1', '2')
-        val emptyList:IndexedSeq[String]
-        val result = combinationsImpl(2, 1, elements, 0, emptyList )
-        printf("result = %s\n", result)*/
-  }
+import scala.collection.immutable.IndexedSeq
 
-/*  def combinationsImpl(n:Int, r:Int, elements:Array[Char], position:Int, comboList:IndexedSeq[String]):IndexedSeq[String] = {
-    printf("Inputs %d %d\n", n, r)
-    val list:IndexedSeq[String] = for (i <- 0 to (n - r -1) )
-     yield combinationsImpl( n - 1, r - 1, elements, position + 1, comboList )
-    return comboList.
-  }*/
+object P051_Prototype {
+
+  //var result:IndexedSeq
+
+  def combinations(digits:List[Int], n:Int, r:Int, result:IndexedSeq[Int]):IndexedSeq[Int] = {
+    if (r == 1) {
+      val result = for (i <- 0 to digits.size - 1 )
+        yield digits(i)
+
+      return result
+    }
+    combinations(digits.tail, n - 1, r - 1, result).map(x => x * 10 + digits.head).union(
+      combinations(digits.tail, n - 1, r - 1, result))
+  }
 }
 
 class P051_Prototype {
   @Test def runTest() {
     println("Run Test")
-    P051_Prototype.combinations(3, 1)
+    println(P051_Prototype.combinations(List(1,2), 2, 1, IndexedSeq()))
+
+    println(P051_Prototype.combinations(List(1,2,3), 3, 1, IndexedSeq()))
+
+    println(P051_Prototype.combinations(List(1,2,3), 3, 2, IndexedSeq()))
     assertEquals(1, 1)
   }
 }
