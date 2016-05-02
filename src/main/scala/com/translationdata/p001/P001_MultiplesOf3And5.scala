b64512bdfccac767f9fb6b89a96ccb859ffe909a
Harold Almon <haroldemail@gmail.com>
2016-05-02 16:58:35 -0500

    Add JUnit tests

--- src/main/scala/com/translationdata/p001/P001_MultiplesOf3And5.scala
+++ src/main/scala/com/translationdata/p001/P001_MultiplesOf3And5.scala
@@ -1,6 +1,9 @@
 package com.translationdata.p001;
-object P001_MultiplesOf3And5 extends App{
-  println("P001: " + multiplesOf3And5(1000))
+import org.junit.Test
+import org.junit.Assert._
+
+object P001_MultiplesOf3And5{
+  def main(args: Array[String]): Int = multiplesOf3And5(1000)
 
   def multiplesOf3And5(upperLimit: Int) = {
     var MultiplesSum = 0
@@ -19,5 +22,13 @@ object P001_MultiplesOf3And5 extends App{
   private def calculateSumBelowLimit(upperLimit: Int, divisor: Int) = {
     val n = (upperLimit - 1) / divisor
     divisor * n * (n + 1) / 2
+  }
+}
+
+class P001_MultiplesOf3And5 {
+  @Test def testMultiplesOf3And5() = {
+    val sum = P001_MultiplesOf3And5.multiplesOf3And5(1000)
+    assertEquals(233168, sum)
+    println("Junit P001: " + sum)
   }
 }
