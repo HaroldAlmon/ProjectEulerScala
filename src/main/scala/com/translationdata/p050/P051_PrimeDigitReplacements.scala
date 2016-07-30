583bf97e065a514dad4dec977304e964d44c9848
Harold Almon <haroldemail@gmail.com>
2016-07-29 21:48:25 -0500

    Add JUnit test

--- src/main/scala/com/translationdata/p050/P051_PrimeDigitReplacements.scala
+++ src/main/scala/com/translationdata/p050/P051_PrimeDigitReplacements.scala
@@ -1,13 +1,15 @@
 package com.translationdata.p050
 
+import org.junit.Test
+
 object P051_PrimeDigitReplacements {
 
  val positions = Array(1,2,3,4)
 
- def main(args: Array[String]): Unit = {
-   positions foreach println
+/*def main(args: Array[String]): Unit = {
+   //positions foreach println
    printCombinations(positions, 4, 2)
- }
+ }*/
 
   def printCombinations (positions: Array[Int], n: Int, r:Int): Unit = {
     printf("n = %d, r = %d%n", n, r)
@@ -32,5 +34,15 @@ object P051_PrimeDigitReplacements {
       else
         printCombinationsImpl(positions, n, r, i + 1,  str + positions(i))
     }
+  }
+}
+
+
+
+class P051_PrimeDigitReplacements {
+  @Test def test = {
+    println("P051:")
+    P051_PrimeDigitReplacements.positions foreach println
+    P051_PrimeDigitReplacements.printCombinations(P051_PrimeDigitReplacements.positions, 4, 2)
   }
 }
