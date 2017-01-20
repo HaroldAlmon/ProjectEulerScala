cee1d8a430940988f50fecc394803854a22acc1a
Harold Almon <haroldemail@gmail.com>
2017-01-20 11:50:14 +0800

    Refactor function name

--- src/main/scala/com/translationdata/p001/P002_EvenFibonacciNumbers.scala
+++ src/main/scala/com/translationdata/p001/P002_EvenFibonacciNumbers.scala
@@ -5,9 +5,9 @@ import org.junit.Assert._
 import org.junit.Test
 ;
 object P002_EvenFibonacciNumbers {
-  def evenFiboNumbersSum(upperLimit: Int ) = evenFiboNumbersSum2_impl(upperLimit, 1, 2, 2)
+  def evenFiboNumbersSum(upperLimit: Int ) = evenFiboNumbersSum_impl(upperLimit, 1, 2, 2)
 
-  def evenFiboNumbersSum2_impl(upperLimit: Int, fibo1: Int, fibo2: Int,  sum: Int ) : Int = {
+  def evenFiboNumbersSum_impl(upperLimit: Int, fibo1: Int, fibo2: Int, sum: Int ) : Int = {
     val nextFibo = fibo1 + fibo2
     if ( nextFibo >= upperLimit )
       return sum
@@ -21,7 +21,7 @@ object P002_EvenFibonacciNumbers {
 
     val nextFibo1 = fibo2
 
-    evenFiboNumbersSum2_impl(upperLimit, nextFibo1, nextFibo, nextSum)
+    evenFiboNumbersSum_impl(upperLimit, nextFibo1, nextFibo, nextSum)
   }
 }
 
