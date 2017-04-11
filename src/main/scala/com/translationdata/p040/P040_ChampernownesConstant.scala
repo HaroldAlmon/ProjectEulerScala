b1b1bde786ffbd5fe1b31986588efb3d7a519472
Harold Almon <haroldemail@gmail.com>
2017-04-10 21:18:53 -0500

    Move tests to test folder

--- src/main/scala/com/translationdata/p040/P040_ChampernownesConstant.scala
+++ src/main/scala/com/translationdata/p040/P040_ChampernownesConstant.scala
@@ -71,20 +71,3 @@ object P040_ChampernownesConstant {
       (product, targetDigitPos)
   }
 }
-
-
-class P040_ChampernownesConstant {
-  @Test def champernownesConstant() = {
-    val product = P040_ChampernownesConstant.champernownesConstant
-
-    checkResult {
-      println("P040: Product d1 x d10 x ... x d1000000 = " + product)
-      assertEquals(210, product)
-    }
-  }
-
-  def checkResult(function: => Unit) {
-    function
-  }
-}
-
